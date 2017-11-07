1d6f480528dee2e21be0fe98e23e985ec5ea8038
Suyash Chopra <suyashchopra19@gmail.com>
2017-11-07 15:16:45 -0500

    We are Stuck

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const nunjucks = require('nunjucks');
+// Importing routes
+const routes = require('./routes/');
+app.use('/', routes);
+
 
 //Configuration for nunjucks
 app.set('view engine', 'html'); // have res.render work with html files
@@ -15,14 +18,14 @@ const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 var count = 0;
 
-app.use("/",(req, res, next)=> {
-	console.log("The site is loaded "+ count++ +" times")
-	next();
-})
+// app.use("/",(req, res, next)=> {
+// 	console.log("The site is loaded "+ count++ +" times")
+// 	next();
+// })
 
-app.get('/', (req, res) => {
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
+// app.get('/', (req, res) => {
+// 	res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
 
 app.listen(3000, () => console.log('To log in go to :"http://localhost:3000/"'))
 
