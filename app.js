77521f52cbcf2bbbc44026a35b9551e1d86256ea
Amanda Hussey <amanda.k.hussey@gmail.com>
2017-11-07 09:48:34 -0600

    finished configuring express

--- app.js
+++ app.js
@@ -2,19 +2,33 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+// Come back to volleyball and morgan, given time
 
 var count=0;
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack)
+//   res.status(500).send('Something broke!')  ASK how this works on server side
+// }) 
+
 app.use("/",(req, res, next)=> {
-	console.log("The page is loaded "+ count++ +" times")
+	console.log("The site is loaded "+ count++ +" times")
+	console.log('Request Type:', req.method)
+	console.log('Request URL:', req.originalUrl)
+	console.log('Status Code: ', res.status(200).statusCode); // ASK diff between res.status vs. res.sendStatus
 	next();
 })
 //localhost:3000/
 
 app.get('/', (req, res) => res.send('Welcome to Express'))
 
-app.get('/news',(req,res) => res.send('This is news!'))
+app.get('/news', (req, res) => res.send('This is news!'))
 
+app.get('/special', (req, res) => res.send('This is a special area!'));
 
+app.get('/special/:id', (req, res) => res.send('This is special area ' + req.params.id + '.'));
+
+app.use("/",(req, res) => res.send('404 Page Not Found'));
 
 app.listen(3000, () => console.log('To log in go to :"http://localhost:3000/"'))
 
