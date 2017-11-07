5e4eaa072823f0458ec5c323daa25fa2cd6a9505
Suyash Chopra <suyashchopra19@gmail.com>
2017-11-07 10:05:24 -0500

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var count=0;
+app.use("/",(req, res, next)=> {
+	console.log("The page is loaded "+ count++ +" times")
+	next();
+})
+//localhost:3000/
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', (req, res) => res.send('Welcome to Express'))
+
+app.get('/news',(req,res) => res.send('This is news!'))
+
+
+
+app.listen(3000, () => console.log('To log in go to :"http://localhost:3000/"'))
+
